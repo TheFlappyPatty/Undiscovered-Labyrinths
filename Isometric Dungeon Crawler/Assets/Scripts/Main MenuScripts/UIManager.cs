80c0b19da31b9f2a5807bf56888f292f42e2bef5
AIE\s229025 <jrverburg@yahoo.com>
2024-02-02 10:15:20 -0800

    Main menu added

---
+++ Isometric Dungeon Crawler/Assets/Scripts/Main MenuScripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Canvas MainMenu;
+    public Canvas Settings;
+    public Canvas LevelSelect;
+
+    public void CloseMenus()
+    {
+        MainMenu.enabled = false;
+        Settings.enabled = false;
+        LevelSelect.enabled = false;
+    }
+    public void SettingsMenu()
+    {
+        CloseMenus();
+        Settings.enabled = true;
+    }
+    public void MainMenuMenu()
+    {
+        CloseMenus();
+        MainMenu.enabled = true;
+    }
+    public void LevelSelectMenu()
+    {
+        CloseMenus();
+        LevelSelect.enabled = true;
+    }
+    public void LoadLevel(String LevelName)
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+}
