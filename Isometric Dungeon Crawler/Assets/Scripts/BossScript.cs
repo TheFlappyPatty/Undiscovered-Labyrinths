bfc957b53d38b150a5cbd90f88051cca1c174405
AIE\s229044 <s229044@students.aie.edu>
2024-02-09 15:36:19 -0800

    Level 2 update

--- Isometric Dungeon Crawler/Assets/Scripts/BossScript.cs
+++ Isometric Dungeon Crawler/Assets/Scripts/BossScript.cs
@@ -63,7 +63,7 @@ public class BossScript : MonoBehaviour
             {
                 foreach (DestructionPillars f in Funky)
                 {
-                    if (f.HealthToDestroy >= BossHealth)
+                    if (f.HealthToDestroy >= bossHealth)
                     {
                         Instantiate(f.DestroyedPillar, f.PillarToDestroy.transform.position, Quaternion.identity,gameObject.transform.parent.transform);
                         Destroy(f.PillarToDestroy);
